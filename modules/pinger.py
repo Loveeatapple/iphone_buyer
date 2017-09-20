1d23308c02c114ec2aecd793f1b031f73923c35f
Jason Ganub <jasonganub@gmail.com>
2017-09-20 17:22:43 +0700

    Created URL and requests library to retrieve the HTML.

--- modules/pinger.py
+++ modules/pinger.py
@@ -1,14 +1,11 @@
-# iPhone 7
-https://www.apple.com/us-hed/shop/buy-iphone/iphone-7/4.7-inch-display-32gb-black-tmobile#00,12,31,40,60
+import requests
+from bs4 import BeautifulSoup
 
-# iphone 6s
-https://www.apple.com/us-hed/shop/buy-iphone/iphone6s/4.7-inch-display-32gb-silver-tmobile#00,12,30,40,60
 
-# iphone 8
-https://www.apple.com/us-hed/shop/buy-iphone/iphone-8/4.7-inch-display-64gb-silver-tmobile#00,12,30,40,60
+# iPhone X 64gb model purchase page
+URL = "https://www.apple.com/us-hed/shop/buy-iphone/iphone-x/5.8-inch-display-64gb-silver-tmobile"
 
-# iphone 8 plus
-https://www.apple.com/us-hed/shop/buy-iphone/iphone-8/5.5-inch-display-64gb-silver-tmobile#01,12,30,40,60
+# Query the URL and store the HTML content
+page = requests.get(URL).text
 
-# iphone X
-https://www.apple.com/us-hed/shop/buy-iphone/iphone-x/5.8-inch-display-64gb-silver-tmobile#00,12,30,40,60
+print(page)
